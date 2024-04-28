e1b137bbc18dfc16785d4f2850ce465b190eff3c
Nick Timms <nicktimms1@hotmail.com>
2024-04-28 13:45:01 -0600

    Added Gallery and Photo functionality

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,9 +1,12 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonIcon, IonFab, IonFabButton } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonIcon, IonFab, IonFabButton, IonGrid, IonImg, IonRow, IonCol } from '@ionic/react';
 import { camera, trash, close } from 'ionicons/icons';
 import ExploreContainer from '../components/ExploreContainer';
+import { usePhotoGallery, UserPhoto } from '../hooks/usePhotoGallery';
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
+  const { takePhoto, photos } = usePhotoGallery();
+  
   return (
     <IonPage>
       <IonHeader>
@@ -12,6 +15,15 @@ const Tab2: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
+        <IonGrid>
+          <IonRow>
+            {photos.map((photo, index) => (
+              <IonCol size="6" key={photo.filepath}>
+                <IonImg src={photo.webviewPath}/>
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
         <IonFab vertical="bottom" horizontal="center" slot="fixed">
           <IonFabButton onClick={() => takePhoto()}>
             <IonIcon icon={camera}></IonIcon>
